afacbf6a86b9af32fc95cf150cc64ca34c99b2f9
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-07 12:00:36 +0200

    Final commit

--- main.py
+++ main.py
@@ -4,30 +4,31 @@ from tkinter import messagebox
 
 root = Tk()
 root.title("BMI Calculator")
-root.geometry("700x500")
-header = Label(root, text='Ideal Body Mass Index Calculator')
-header.place(x=250, y=0)
+root.geometry("700x600")
+root.config(bg="#DDFFF7")
+header = Label(root, text='IDEAL BODY MASS INDEX CALCULATOR', bg='#EED3CB', fg='black')
+header.place(x=220, y=20)
 
-frame = Frame(root, width=500, height=200, borderwidth=1, relief='ridge')
+frame = Frame(root, width=500, height=200, relief='raised', bg='#EED3CB')
 frame.place(relx=0.15, rely=0.1)
 
-weight = Label(frame, text="Weight(kg):")
-weight.place(relx=0, rely=0)
+weight = Label(frame, text="Weight(kg):", bg='#EED3CB', fg='black')
+weight.place(relx=0.1, rely=0.1)
 weight_entry = Entry(frame)
-weight_entry.place(relx=0.3, rely=0)
+weight_entry.place(relx=0.4, rely=0.1)
 
-height = Label(frame, text="Height(cm):")
-height.place(relx=0, rely=0.2)
+height = Label(frame, text="Height(cm):", bg='#EED3CB', fg='black')
+height.place(relx=0.1, rely=0.3)
 height_entry = Entry(frame)
-height_entry.place(relx=0.3, rely=0.2)
+height_entry.place(relx=0.4, rely=0.3)
 
-gender = Label(frame, text="Gender:")
-gender.place(rely=0.43, relx=0)
+gender = Label(frame, text="Gender:", bg='#EED3CB', fg='black')
+gender.place(rely=0.53, relx=0.1)
 
-age = Label(frame, text="Age:")
-age.place(rely=0.7, relx=0)
+age = Label(frame, text="Age:", bg='#EED3CB', fg='black')
+age.place(rely=0.8, relx=0.1)
 age_entry = Entry(frame, state='readonly')
-age_entry.place(rely=0.7, relx=0.3)
+age_entry.place(rely=0.8, relx=0.4)
 
 options = ['Select...', 'Male', "Female"]
 variable = StringVar(frame)
@@ -43,7 +44,7 @@ def activate(value):
 
 
 gender_menu = OptionMenu(frame, variable, *options, command=activate)
-gender_menu.place(relx=0.3, rely=0.4)
+gender_menu.place(relx=0.4, rely=0.5)
 
 
 def bmi_calc():
@@ -74,22 +75,31 @@ def bmi_calc():
             bmi_field.config(state='normal')
             bmi_field.insert(0, round(result_bmi, 1))
             bmi_field.config(state='readonly')
+        if result_bmi < 18.5:
+            category.config(text='Underweight')
+        elif 18.5 <= result_bmi < 25:
+            category.config(text='Normal weight')
+        elif 25 <= result_bmi < 30:
+            category.config(text='Overweight')
+        elif result_bmi >= 30:
+            category.config(text='Obese')
+
     except ValueError:
         messagebox.showerror(title=None, message='Gender was not specified or invalid entry was given')
         delete()
 
 
 calculate = Button(root, text="Calculate your Ideal Body Mass Index", width=50, command=bmi_calc)
-calculate.place(rely=0.52, relx=0.2)
+calculate.place(rely=0.45, relx=0.2)
 
-bmi = Label(root, text="BMI:")
-bmi.place(rely=0.7, relx=0.1)
+bmi = Label(root, text="BMI:", bg='#EED3CB', fg="black")
+bmi.place(rely=0.55, relx=0.1)
 bmi_field = Entry(root, state='readonly')
-bmi_field.place(rely=0.7, relx=0.2)
-ideal_bmi = Label(root, text='Ideal BMI:')
-ideal_bmi.place(rely=0.7, relx=0.5)
+bmi_field.place(rely=0.55, relx=0.2)
+ideal_bmi = Label(root, text='Ideal BMI:', bg='#EED3CB', fg="black")
+ideal_bmi.place(rely=0.55, relx=0.5)
 ideal_field = Entry(root, state='readonly')
-ideal_field.place(rely=0.7, relx=0.65)
+ideal_field.place(rely=0.55, relx=0.65)
 
 
 def delete():
@@ -108,6 +118,10 @@ def delete():
     variable.set(options[0])
 
 
+category_head = Label(root, text="Category:", bg='#EED3CB', fg='black')
+category = Label(root, width=20, bg='#EED3CB', fg='black')
+category.place(relx=0.38, rely=0.72)
+category_head.place(relx=0.45, rely=0.67)
 clear = Button(root, text='Clear', command=delete)
 clear.place(rely=0.85, relx=0.1)
 quit = Button(root, text='Exit', command='exit')
